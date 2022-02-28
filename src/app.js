18d2319748bdae7bc205fa541ccab752e2482ebb
klouo <97315126+klouo@users.noreply.github.com>
2022-02-28 11:25:22 +1100

    Added in framework for displayForecast function

--- src/app.js
+++ src/app.js
@@ -21,7 +21,8 @@ let day = days[date.getDay()];
 return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast(){
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
   let days = ["Thu", "Fri", "Sat", "Sun"];
 
@@ -49,6 +50,12 @@ function displayForecast(){
   console.log(forecastHTML);
 }
 
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function showWeather(response) {
   console.log(response);
   document.querySelector("#city").innerHTML = response.data.name;
@@ -71,35 +78,23 @@ function showWeather(response) {
   iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
   celsiusTemperature = response.data.main.temp;
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  getForecast(response.data.coord);
 }
 
 function searchCity(cityInput) {
-let units = "metric";
-let apiKey = "f8f0c9241ff771e67ddebb64996017c7";
 let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
 let apiUrl = `${apiEndpoint}?q=${cityInput}&appid=${apiKey}&units=${units}`;
 axios.get(apiUrl).then(showWeather);
 }
 
-function locationSearch(event) {
-  event.preventDefault();
-  let cityInput = document.querySelector("#city-input").value;
-  searchCity(cityInput);
-}
-
-function updateWeather(position) {
-  let latitude = position.coords.latitude;
-  let longitude = position.coords.longitude;
-  let units = "metric";
-  let apiKey = "f8f0c9241ff771e67ddebb64996017c7";
-  let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
-  let apiUrl = `${apiEndpoint}?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
+function showLocation(position) {
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(showWeather);
 }
 
 function retrievePosition(event) {
   event.preventDefault();
-  navigator.geolocation.getCurrentPosition(updateWeather);
+  navigator.geolocation.getCurrentPosition(showLocation);
 }
 
 function convertToFahrenheit(event) {
@@ -119,19 +114,30 @@ function displayCelsius(event) {
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
 
+function locationSearch(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#city-input").value;
+  searchCity(cityInput);
+}
+
+let apiKey = "f8f0c9241ff771e67ddebb64996017c7";
+let units = "metric";
 let celsiusTemperature = null;
 
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", convertToFahrenheit);
+
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsius);
+
 let currentLocation = document.querySelector("#current-location-button");
 currentLocation.addEventListener("click", retrievePosition);
+
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", locationSearch);
+
 let iconElement = document.querySelector("#icon");
 let temperatureElement = document.querySelector("#temperature");
 let dateElement = document.querySelector("#date");
 
 searchCity("Queenstown");
-displayForecast();
